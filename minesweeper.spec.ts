0f9701e33647ced7d28c74ffa057d1e6a9141630
recca0120 <recca0120@gmail.com>
2017-06-04 18:36:01 +0800

    地雷地圖建立

--- minesweeper.spec.ts
+++ minesweeper.spec.ts
@@ -1,1 +1,22 @@
 import { Minesweeper } from './minesweeper';
+describe('踩地雷', () => {
+    it('建立新遊戲', () => {
+        const cols = 8;
+        const rows = 5;
+        const mines = 10;
+        const sweeper = new Minesweeper(cols, rows, mines);
+        const map = sweeper.getMap();
+        let counts = 0;
+        map.forEach((rows) => {
+            rows.forEach((item) => {
+                if (item == true) {
+                    counts++;
+                }
+            });
+        })
+        expect(map).toEqual(expect.any(Array));
+        expect(map.length).toEqual(cols);
+        expect(map[0].length).toEqual(rows);
+        expect(counts).toEqual(mines);
+    });
+});
