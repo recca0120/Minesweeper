4108da600e61b60b1477fecd905a3cb13ac914a6
recca0120 <recca0120@gmail.com>
2017-06-04 21:34:06 +0800

    計算周圍炸彈數

---
+++ point.spec.ts
@@ -0,0 +1,40 @@
+import { Point } from './point';
+
+describe('點', () => {
+    function createMap() {
+        const rows = 5;
+        const cols = 5;
+        const map = (new Array(rows)).fill(null).map(() => (new Array(cols)).fill(false));
+        map[1][3] = true;
+        map[2][1] = true;
+        map[2][3] = true;
+        map[3][1] = true;
+        map[3][2] = true;
+        map[3][3] = true;
+        map[4][4] = true;
+
+        return map;
+    }
+
+    it('是炸彈', () => {
+        const fakeMap = createMap();
+        expect(new Point(1, 3, fakeMap).isMine()).toBe(true);
+        expect(new Point(2, 1, fakeMap).isMine()).toBe(true);
+        expect(new Point(4, 4, fakeMap).isMine()).toBe(true);
+    });
+
+    it('非炸彈', () => {
+        const fakeMap = createMap();
+        expect(new Point(1, 4, fakeMap).isMine()).toBe(false);
+        expect(new Point(2, 4, fakeMap).isMine()).toBe(false);
+        expect(new Point(3, 4, fakeMap).isMine()).toBe(false);
+    });
+
+    it('計算炸彈數', () => {
+        const fakeMap = createMap();
+        const point = new Point(2, 2, fakeMap);
+
+        expect(point.isMine()).toBe(false);
+        expect(point.getNumber()).toEqual(6);
+    });
+});
--- point.ts
+++ point.ts
@@ -5,10 +5,37 @@ export class Point {
     private _isMine: boolean;
 
     constructor(private x, private y, private map: Array<Array<boolean>>) {
-        this._isMine = this.map[x][y] === true;
+        this._isMine = this.map[x][y];
     }
 
-    isMine() {
+    public isMine() {
         return this._isMine;
+    }
+
+    public getNumber() {
+        const positions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+        ];
+
+        return positions.map(([x, y]) => {
+            console.log(this.getPosition(x, y), x, y);
+            return this.getPosition(x, y);
+        }).filter(x => x === true).length;
+    }
+
+    protected getPosition(x: number, y: number) {
+        if (this.map[x] === undefined || this.map[x][y] === undefined) {
+            return null;
+        }
+
+        return this.map[x][y];
     }
 }
